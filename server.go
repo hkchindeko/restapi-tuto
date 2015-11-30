b40c91783f0dabc171675ae2c275364b92cf7167
hkchindeko <laurent.chindeko.hk@gmail.com>
2015-11-30 21:50:00 +0800

    Add a route to to the server. Just a basic root to serve /

--- server.go
+++ server.go
@@ -5,12 +5,16 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString((r.URL.Path)))
-	})
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
-	log.Fatal(http.ListenAndServe(":9080", nil))
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":9080", router))
 }
