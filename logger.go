dd08a268059b943d771033f5f2a3a300c6628068
hkchindeko <laurent.chindeko.hk@gmail.com>
2015-12-02 02:19:39 +0800

    Add a Database Mock Add Id property in the Todo object Update the TodoIndex handler Add a new Route to post json Create an endpoint handler

--- logger.go
+++ logger.go
@@ -3,18 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		l := log.New(os.Stdout, "[Web API]", 0)
 		start := time.Now()
 
 		inner.ServeHTTP(w, r)
 
-		l.Printf(
+		log.Printf(
 			"%s\t%s\t%s\t%s",
 			r.Method,
 			r.RequestURI,
